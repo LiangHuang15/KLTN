d5c59343a09caa28748026a68177a3f8e02fe6df
PhanNhan123 <ptnhan10091999@gmail.com>
2021-07-18 11:00:24 +0700

    123

--- Neighborhood.py
+++ Neighborhood.py
@@ -9,7 +9,7 @@ import time
 start_time = time.time()
 list =[]
 class CF(object):
-    """docstring for CF"""
+
     def __init__(self, Y_data, k, dist_func = cosine_similarity, uuCF = 1):
         self.uuCF = uuCF # user-user (1) or item-item (0) CF
         self.Y_data = Y_data if uuCF else Y_data[:, [1, 0, 2]]
@@ -21,22 +21,21 @@ class CF(object):
         self.n_items = int(np.max(self.Y_data[:, 1])) + 1
     
     def add(self, new_data):
-        """
-        Update Y_data matrix when new ratings come.
-        For simplicity, suppose that there is no new user or item.
-        """
+
         # ghép mảng axis = 0 đưa giá trị của mảng ghép vào đưa vào cuối 
         self.Y_data = np.concatenate((self.Y_data, new_data), axis = 0)
     
     def normalize_Y(self):
-        users = self.Y_data[:, 0] # all users - first col of the Y_data
+        users = self.Y_data[:, 0] # tất cả user 
         self.Ybar_data = self.Y_data.copy()
+        # mảng 0 với n cột 
         self.mu = np.zeros((self.n_users,))
         for n in range(self.n_users):
+            # những dòng mà user là n 
             ids = np.where(users == n)[0].astype(np.int32)
-            # indices of all ratings associated with user n
+            # những sản phẩm được user n ratings 
             item_ids = self.Y_data[ids, 1] 
-            # and the corresponding ratings 
+            # ratings tương ứng với user n
             ratings = self.Y_data[ids, 2]
             m = np.mean(ratings) 
             if np.isnan(m):
@@ -56,27 +55,20 @@ class CF(object):
     
         
     def refresh(self):
-        """
-        Normalize data and calculate similarity matrix again (after
-        some few ratings added)
-        """
+
         self.normalize_Y()
         self.similarity() 
         
     def fit(self):
         self.refresh()
-        
+    ## dự đoán ratings user cho item i 
     def __pred(self, u, i, normalized = 1):
-        """ 
-        predict the rating of user u for item i (normalized)
-        if you need the un
-        """
-        # Step 1: find all users who rated i
+
+        #  tìm tất cả các user đã ratings cho i 
         ids = np.where(self.Y_data[:, 1] == i)[0].astype(np.int32)
-        # Step 2: 
+        # những ngừoi dùng đã rating
         users_rated_i = (self.Y_data[ids, 0]).astype(np.int32)
-        # Step 3: find similarity btw the current user and others 
-        # who already rated i
+        # tìm độ tương đồng giữa người dùng u với những người dùng khác 
         sim = self.S[u, users_rated_i]
         # Step 4: find the k most similarity users
         a = np.argsort(sim)[-self.k:] 
@@ -91,25 +83,17 @@ class CF(object):
         return (r*nearest_s)[0]/(np.abs(nearest_s).sum() + 1e-8) + self.mu[u]
     
     def pred(self, u, i, normalized = 1):
-        """ 
-        predict the rating of user u for item i (normalized)
-        if you need the un
-        """
+
         if self.uuCF: return self.__pred(u, i, normalized)
         return self.__pred(i, u, normalized)
             
     def recommend(self, u):
-        """
-        Determine all items should be recommended for user u.
-        The decision is made based on all i such that:
-        self.pred(u, i) > 0. Suppose we are considering items which 
-        have not been rated by u yet. 
-        """
+
         ids = np.where(self.Y_data[:, 0] == u)[0]
         items_rated_by_u = self.Y_data[ids, 1].tolist()     
      
         recommended_items = []
-        for i in range(self.n_items):
+        for i in range(1,self.n_items,1):
             if i not in items_rated_by_u:
                 rating = self.__pred(u, i)
                 if rating > 0: 
@@ -119,12 +103,7 @@ class CF(object):
         return recommended_items 
     
     def recommend2(self, u):
-        """
-        Determine all items should be recommended for user u.
-        The decision is made based on all i such that:
-        self.pred(u, i) > 0. Suppose we are considering items which 
-        have not been rated by u yet. 
-        """
+
         ids = np.where(self.Y_data[:, 0] == u)[0]
         items_rated_by_u = self.Y_data[ids, 1].tolist()              
         recommended_items = []
@@ -133,16 +112,12 @@ class CF(object):
             if i not in items_rated_by_u:
                 rating = self.__pred(u, i)
                 if rating > 0: 
-                    recommended_items.append(i)
-                
-        
+                    recommended_items.append(i)    
         
         return recommended_items 
 
     def print_recommendation(self):
-        """
-        print all items which should be recommended for each user 
-        """
+
         # print ('Recommendation: ')
         # for u in range(self.n_users):
         #     recommended_items = self.recommend(u)
@@ -164,13 +139,6 @@ class CF(object):
                 print ('    Recommend item', u, 'for user(s) : ', recommended_items)
 
 
-# r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-
-# ratings_base = pd.read_csv('ml-100k/ub.base', sep='\t', names=r_cols, encoding='latin-1')
-# ratings_test = pd.read_csv('ml-100k/ub.test', sep='\t', names=r_cols, encoding='latin-1')
-
-# print('type',ratings_base.dtypes)
-
 import pymysql 
 conn =pymysql.connect(host="localhost",user="root",passwd="",database="movielens")
 cursor = conn.cursor()
@@ -190,18 +158,13 @@ rate_train = Ratings_base.values
 rate_test = Ratings_test.values
 
 
-
-
 pd.set_option('display.max_rows',1000000)
 pd.set_option('display.max_columns',5)
-# rate_train = ratings_base.values
-# rate_test = ratings_test.values
-
 
 print('rate1',rate_train)
 print('rate2',rate_test)
-rate_train[:, :2] -= 1
-rate_test[:, :2] -= 1
+# rate_train[:, :2] -= 1
+# rate_test[:, :2] -= 1
 print('rate_train',rate_train[:, :2])
 print('rate_test',rate_test[:, :2])
 
@@ -221,11 +184,11 @@ with open('/Applications/XAMPP/xamppfiles/htdocs/KLTN/data.json') as json_file:
 print('recommend for user: ',u)
 print ('User-user CF, RMSE =', RMSE)
 rs.print_recommendation()
-print('list',list)
+# print('list',list)
 
 conn =pymysql.connect(host="localhost",user="root",passwd="",database="movielens")
 cursor = conn.cursor()
-sql1="delete from recommend where UserID = %s"
+sql1="delete from test_recommend where UserID = %s"
 cursor.execute(sql1,(int(u)))
 for i in range(0,10,1):
     try:
@@ -237,25 +200,4 @@ conn.commit()
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
-
-
-
-
-
-
-# rs = CF(rate_train, k = 30, uuCF = 0)
-# rs.fit()
-
-# n_tests = rate_test.shape[0]
-# SE = 0 # squared error
-# for n in range(n_tests):
-#     pred = rs.pred(rate_test[n, 0], rate_test[n, 1], normalized = 0)
-#     SE += (pred - rate_test[n, 2])**2 
-
-# RMSE = np.sqrt(SE/n_tests)
-# with open('/Applications/XAMPP/xamppfiles/htdocs/KLTN/data.json') as json_file:
-#     userdata = json.load(json_file)
-#     u=userdata['username']
-# print('recommend for user: ',u)
-# print ('Item-item CF, RMSE =', RMSE)
 
